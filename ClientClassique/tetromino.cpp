b0b0a77272bb206b921be6026e7ce63b0406d57c
Aranud <aranud66@live.fr>
2015-01-09 14:27:33 +0100

    Client Classique : classe Tétromino
    Classe définissent les tétrominos permettant de jouer au Tetris

--- ClientClassique/tetromino.cpp
+++ ClientClassique/tetromino.cpp
@@ -12,42 +12,49 @@ Tetromino::Tetromino(eTetromino p_eTetromino, QObject* parent) :
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(1, 0));
             m_lpListPoint.append(QPoint(2, 0));
+            m_cColor = Qt::cyan;
         break;
         case eTetrominoO:
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(1, 0));
             m_lpListPoint.append(QPoint(0, 1));
             m_lpListPoint.append(QPoint(1, 1));
+            m_cColor = Qt::yellow;
         break;
         case eTetrominoT:
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(-1, 1));
             m_lpListPoint.append(QPoint(0, 1));
             m_lpListPoint.append(QPoint(1, 1));
+            m_cColor = Qt::magenta;
         break;
         case eTetrominoJ:
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(-1, 0));
             m_lpListPoint.append(QPoint(1, 0));
             m_lpListPoint.append(QPoint(1, 1));
+            m_cColor = Qt::blue;
         break;
         case eTetrominoL:
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(-1, 0));
             m_lpListPoint.append(QPoint(1, 0));
             m_lpListPoint.append(QPoint(-1, 1));
+            m_cColor = Qt::black;
         break;
         case eTetrominoS:
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(1, 0));
             m_lpListPoint.append(QPoint(0, 1));
             m_lpListPoint.append(QPoint(-1, 1));
+            m_cColor = Qt::green;
         break;
         case eTetrominoZ:
             m_lpListPoint.append(QPoint(-1, 0));
             m_lpListPoint.append(QPoint(0, 0));
             m_lpListPoint.append(QPoint(0, 1));
             m_lpListPoint.append(QPoint(1, 1));
+            m_cColor = Qt::red;
         break;
         default:
             break;
@@ -57,4 +64,24 @@ Tetromino::Tetromino(eTetromino p_eTetromino, QObject* parent) :
 Tetromino::~Tetromino()
 {
 
+}
+
+QList<QPoint> Tetromino::getListPoint() const
+{
+    return m_lpListPoint;
+}
+
+void Tetromino::setListPoint(const QList<QPoint> &lpListPoint)
+{
+    m_lpListPoint = lpListPoint;
+}
+
+QColor Tetromino::getColor() const
+{
+    return m_cColor;
+}
+
+void Tetromino::setColor(const QColor &cColor)
+{
+    m_cColor = cColor;
 }
--- ClientClassique/tetromino.h
+++ ClientClassique/tetromino.h
@@ -1,17 +1,24 @@
 #ifndef PIECE_H
 #define PIECE_H
 
-#include <headergloabal.h>
+#include "headergloabal.h"
 
 class Tetromino : QObject
 {
     Q_OBJECT
 private:
     QList<QPoint> m_lpListPoint;
+    QColor m_cColor;
 
 public:
     explicit Tetromino(eTetromino p_eTetromino, QObject *parent = 0);
     ~Tetromino();
+
+    QList<QPoint> getListPoint() const;
+    void setListPoint(const QList<QPoint> &lpListPoint);
+
+    QColor getColor() const;
+    void setColor(const QColor &cColor);
 
 signals:
 
